801fc8eff1b0c12d66a895d400e13b74ad16fdfd
joe pickrell <joepickrell@gmail.com>
2016-07-09 17:32:41 +0000

    faster loading

--- load-all.py
+++ load-all.py
@@ -1,11 +1,17 @@
 import sys, os
 
-for i in range(1, 25):
-	c = str(i)
-	if c == "23":
-		c = "X"
-	elif c == "24":
-		c = "Y"
-	cmd = "tabix -h ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz "+c+" | python3 load-db.py"
-	print(cmd)
-	os.system(cmd)
+import run
+
+run.db.create_all()
+cmd = "curl ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz | gunzip - | python3 load-db.py"
+print(cmd)
+os.system(cmd)
+#for i in range(1, 25):
+#	c = str(i)
+#	if c == "23":
+#		c = "X"
+#	elif c == "24":
+#		c = "Y"
+#	cmd = "tabix -h ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz "+c+" | python3 load-db.py"
+#	print(cmd)
+#	#os.system(cmd)
--- load-db.py
+++ load-db.py
@@ -2,13 +2,15 @@ import re, sys, os, gzip
 
 import run
 
-run.db.create_all()
+#run.db.create_all()
 
 #infile = gzip.open(sys.argv[1])
 infile = sys.stdin
 line = infile.readline()
 vep_field_names = []
 index = 0
+v_index = 0
+a_index = 0
 while line:
 	#line = bytes.decode(line)
 	if line[0] == "#":
@@ -25,18 +27,21 @@ while line:
 	#print(info_field)
 	annotations = [dict(zip(vep_field_names, x.split('|'))) for x in info_field['CSQ'].split(',')]
 	#print(annotations)
-	v = run.Variant(chr = fields[0], pos = fields[1], rsid = fields[2], ref= fields[3], alt = fields[4], AC_AFR = info_field['AC_AFR'], AN_AFR = info_field['AN_AFR'], AC_AMR = info_field['AC_AMR'], AN_AMR = info_field['AN_AMR'], AC_EAS = info_field['AC_EAS'], AN_EAS = info_field['AN_EAS'], AC_FIN = info_field['AC_FIN'], AN_FIN = info_field['AN_FIN'], AC_NFE = info_field['AC_NFE'], AN_NFE = info_field['AN_NFE'], AC_OTH =  info_field['AC_OTH'], AN_OTH = info_field['AN_OTH'], AC_SAS = info_field['AC_SAS'], AN_SAS = info_field['AN_SAS'])
+	v = run.Variant(id = v_index+1, chr = fields[0], pos = fields[1], rsid = fields[2], ref= fields[3], alt = fields[4], AC_AFR = info_field['AC_AFR'], AN_AFR = info_field['AN_AFR'], AC_AMR = info_field['AC_AMR'], AN_AMR = info_field['AN_AMR'], AC_EAS = info_field['AC_EAS'], AN_EAS = info_field['AN_EAS'], AC_FIN = info_field['AC_FIN'], AN_FIN = info_field['AN_FIN'], AC_NFE = info_field['AC_NFE'], AN_NFE = info_field['AN_NFE'], AC_OTH =  info_field['AC_OTH'], AN_OTH = info_field['AN_OTH'], AC_SAS = info_field['AC_SAS'], AN_SAS = info_field['AN_SAS'])
 	run.db.session.add(v)
-	run.db.session.flush()
+	
 	for annot in annotations:
 		impact = annot['IMPACT']
 		if impact in set(["HIGH", "MODERATE", "LOW"]):
-			a = run.Annotation(variant_id = v.id, allele = annot['Allele'], consequence = annot['Consequence'], symbol = annot['SYMBOL'], gene = annot['Gene'], lof = annot["LoF"], lof_filter = annot['LoF_filter'], lof_flags = annot['LoF_flags'])
+			a = run.Annotation(id = a_index+1, variant_id = v_index+1, allele = annot['Allele'], consequence = annot['Consequence'], symbol = annot['SYMBOL'], gene = annot['Gene'], lof = annot["LoF"], lof_filter = annot['LoF_filter'], lof_flags = annot['LoF_flags'])
 			run.db.session.add(a)
-	run.db.session.commit()
+			a_index = a_index+1
 	if index % 1000 ==0:
+		# commit only every 1000 variants
+		run.db.session.commit()
 		print ("READ "+str(index)+" VARIANTS")
 		print ("CHR "+fields[0]+" POS "+fields[1])
 	index = index+1
+	v_index = v_index+1
 	line = infile.readline()
 	
